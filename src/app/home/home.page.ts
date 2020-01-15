e51654010df17ba829228ed0ab4780a4471a84e5
VitorAnjosSantos <vitor.anjos.santoss@gmail.com>
2020-01-15 10:42:24 -0300

    Criando barra de pesquisa

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,14 +10,54 @@ export class HomePage {
   
   contatos: any;
 
+  pessoa: Array <{codigo: any; nome: string; telefone: string; email: string}>
+
+  pessoaTodos: Array <{codigo: any; nome: string; telefone: string; email: string}>
+
   constructor(public navCtrl: NavController, public servidor: SevidorProviderService) {
 
+    this.pessoa = [];
     this.getRetornar();
   }
 
   getRetornar(){
-    this.servidor.getPegar().subscribe(data => this.contatos = data,err =>console.log(err));
+    this.servidor.getPegar()
+      .subscribe(
+        data => {
+          
+          this.contatos = data;
+        
+          for(let i = 0; i < data.length; i++){
+            
+            this.pessoa.push({
+              codigo: data[i]["codigo"],
+              nome: data[i]["nome"],
+              telefone: data[i]["telefone"],
+              email: data[i]['email']
+            });
+          }
+          this.pessoaTodos = this.pessoa;
+        }
+        
+      )
+        err =>console.log(err);
   }
+        getContatos(ev: any) {
+      
+          // set val to the value of the searchbar
+          const val = ev.target.value;
+      
+          // if the value is an empty string don't filter the items
+          if (val && val.trim() != '') {
+            this.pessoa = this.pessoaTodos.filter((contatos) => {
+              return (contatos.nome.toLowerCase().indexOf(val.toLowerCase()) > -1);
+            })
+          }
+          else{
+            this.pessoa = this.pessoaTodos;
+          }
+        }
+  
 
 
 }
